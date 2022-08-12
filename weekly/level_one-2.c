7591e1ae06a9568f83fa970757b70eb1f8788a99
nnongta <nita290646@gmail.com>
2022-08-12 16:21:32 +0700

    what

---
+++ weekly/level_one-2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    int arr[5],n;
+    for(int i=0;i<5;i++){
+        scanf("%d" ,&arr[i]);
+    }
+    for(int i=0;i<5;i++){
+        scanf("%d" ,&n);
+        for(int j=0;j<5;j++)
+        {
+            if(n==arr[j])
+            {
+                printf("%d ",n);
+            }
+        }
+    }
+}
