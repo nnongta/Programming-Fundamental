d75ffe8bed4485256a27d4580c74fb66354d52e7
nnongta <nita290646@gmail.com>
2022-09-17 18:03:39 +0700

    new solution

--- weekly/week 7/solution_one.c
+++ weekly/week 7/solution_one.c
@@ -1,24 +1,49 @@
 #include <stdio.h>
-
-// check palindrome
-int main() {
-  int n, reversed = 0, remainder, original;
-    printf("Enter an integer: ");
-    scanf("%d", &n);
-    original = n;
-
+int main()
+{
+    int n , m , sum ;
+    int max = 0 ;
+    int pig[n+1] , check[m+1] ;
+    int mem[m+1] ;
     
-    while (n != 0) {
-        remainder = n % 10;
-        reversed = reversed * 10 + remainder;
-        n /= 10;
+    printf("input n :");
+    scanf("%d",&n);
+    printf("input m :");
+    scanf("%d",&m);
+
+    for ( int i = 0 ; i < n ; i++ )  
+    {
+        scanf( "%d" , &pig[i] );
     }
 
-    
-    if(original == reversed)
-        printf("%d is a palindrome.", original);
-    else
-        printf("%d is not a palindrome.", original);
+    for ( int i = 0 ; i < n - m + 1 ; i++ )
+    {
+        sum = 0 ;
+
+        for ( int j = 0 ; j < m ; j++ ) 
+        {
+            check[j] = pig[i+j] ;
+        }
+
+        for ( int k = 0 ; k < m ; k++ ) 
+        {
+            sum = sum + check[k] ;
+        }
+
+        if( sum >= max )  
+        {
+            max=sum ;
+            for ( int r = 0 ; r < m ; r++ ) 
+            {
+                mem[r]=check[r] ;
+            }
+        }
+    }
+
+    for ( int p = 0 ; p < m ; p++ )  
+    {
+        printf ( "%d " , mem[p] ) ;
+    }
 
     return 0;
 }
--- weekly/week 7/solution_two.c
+++ weekly/week 7/solution_two.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
-
 int main()
 {
-int n, reversedInteger = 0  , remainder, originalInteger;
-printf("Enter an integer: ");
-scanf("%d", &n);
-originalInteger = n;
-// reversed integer is stored in variable (#1)
+    int n,m;
+    printf("n = ");
+    scanf("%d",&n);
+    printf("m = ");
+    scanf("%d",&m);
 
-while( n!=0 )
-{
-remainder = n%10;
-reversedInteger = reversedInteger*10 + remainder;
-n /= 10; //(#2)
-}
-
-//printf("%d\n", reversedInteger);
-// palindrome if orignalInteger and reversedInteger are equal (#3)
-if (originalInteger == reversedInteger)
-printf("%d is a palindrome.", originalInteger);
-else
-printf("%d is not a palindrome.", originalInteger);
-return 0;
+    int pig[n+1];
+    pig[0]=0;
+    for(int i=1;i<=n;i++)
+        scanf("%d",&pig[i]);
+    
+    int qs[n+1];
+    qs[0]=0;
+    for(int i=1;i<=n;i++)
+        qs[i]=qs[i-1]+pig[i];
+    int max=qs[m];
+    int mem=m;
+    for(int i=m+1;i<=n;i++)
+    {
+        if(max<qs[i]-qs[i-m])
+        {
+            max=qs[i]-qs[i-m];
+            mem=i;
+        }
+    }
+    for(int i=mem-m+1;i<=mem;i++)
+        printf("\n%d",pig[i]);
 }
